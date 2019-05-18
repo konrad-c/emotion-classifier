f33dc77f223c51dbe68fe20f8663146fa40010ce
konrad-c <ko.cybulski@gmail.com>
2019-05-18 21:35:01 +1000

    Converted classifier script to keras

--- classifier.py
+++ classifier.py
@@ -1,13 +1,12 @@
-import boto3
-import os
 import argparse
-import json
+import os
 import pathlib
 
-from tensorflow.python.lib.io import file_io
-from tensorflow.python.estimator.export.export import build_raw_serving_input_receiver_fn
-from tensorflow.python.estimator.export.export_output import PredictOutput
 import tensorflow as tf
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.losses import sparse_categorical_crossentropy
+from tensorflow.python.keras.models import Model
+from tensorflow.python.keras.optimizers import Adam
 
 INPUT_TENSOR_NAME = "image"
 SIGNATURE_NAME = "serving_default"
@@ -20,88 +19,44 @@ IMG_SHAPE = [IMG_SIZE, IMG_SIZE, 3]
 OUTPUT_SHAPE = 9
 
 
-def model_fn(features, labels, mode, params):
-    """
-    Model function for Estimator.
-     # Logic to do the following:
-     # 1. Configure the model via Keras functional api
-     # 2. Define the loss function for training/evaluation using Tensorflow.
-     # 3. Define the training operation/optimizer using Tensorflow operation/optimizer.
-     # 4. Generate predictions as Tensorflow tensors.
-     # 6. Return predictions/loss/train_op/eval_metric_ops in EstimatorSpec object
-     """
-
-    # 1. Configure the model via Keras functional api
+def build_classifier():
     # Create the base model from the pre-trained ResNet50 network
     base_model = tf.keras.applications.ResNet50(input_shape=IMG_SHAPE,
                                                 include_top=False,
-                                                # pooling="avg",
-                                                weights='imagenet')(features[INPUT_TENSOR_NAME])
+                                                pooling="avg",
+                                                weights='imagenet')
     base_model.trainable = False
-    pooling = tf.keras.layers.GlobalAveragePooling2D()(base_model)
-    output_layer = tf.keras.layers.Dense(OUTPUT_SHAPE, activation='softmax')(pooling)
-
-    predictions = tf.reshape(output_layer, [1, OUTPUT_SHAPE])
-
-    # Provide an estimator spec for `ModeKeys.PREDICT`.
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(
-            mode=mode,
-            predictions={"emotion": predictions},
-            export_outputs={SIGNATURE_NAME: PredictOutput({"emotion": predictions})})
-
-    # 2. Define the loss function for training/evaluation using Tensorflow.
-    cross_entropy = tf.losses.softmax_cross_entropy(labels, predictions)
-
-    # 3. Define the training operation/optimizer using Tensorflow operation/optimizer.
-    optimizer = tf.train.RMSPropOptimizer(learning_rate=params["learning_rate"])
-
-    train_op = optimizer.minimize(
-        loss=cross_entropy,
-        global_step=tf.train.get_global_step())
-
-    # 4. Generate necessary evaluation metrics.
-    # label_tensor = tf.cast(labels, tf.float32)
-    # accuracy, accuracy_update_op = tf.metrics.accuracy(labels=tf.argmax(label_tensor), predictions=tf.argmax(predictions), name='accuracy_op')
-    # metrics = {
-    #     'accuracy': (accuracy, accuracy_update_op),
-    #     'cross_entropy': (cross_entropy, train_op)
-    # }
-
-    # Provide an estimator spec for `ModeKeys.EVAL` and `ModeKeys.TRAIN` modes.
-    return tf.estimator.EstimatorSpec(
-        mode=mode,
-        loss=cross_entropy,
-        train_op=train_op)
-        # eval_metric_ops=metrics)
-
-
-def serving_input_fn():
-    tensor = tf.placeholder(tf.float32, shape=IMG_SHAPE)
-    receiver_tensors = { INPUT_TENSOR_NAME: tensor }
-    return tf.estimator.export.ServingInputReceiver(features, receiver_tensors)
+    x = base_model.output
+    x = Dense(512, activation='relu')(x)
+    preds = Dense(OUTPUT_SHAPE, activation='softmax')(x)
+    return Model(inputs=base_model.inputs, outputs=preds)
 
 
 def _input_fn(directory,
-            glob="*",
-            limit=None,
-            buffer_size=2 * BATCH_SIZE,
-            batch_size=BATCH_SIZE,
-            prefetch_batch_num=2):
+              glob="*",
+              limit=None,
+              buffer_size=2 * BATCH_SIZE,
+              batch_size=BATCH_SIZE,
+              prefetch_batch_num=2):
     filepaths = map(lambda x: str(x), pathlib.Path(directory).glob(glob))
     filepaths = list(filepaths)
     files = tf.data.Dataset.from_tensor_slices(filepaths)
     if limit is not None:
         files = files.take(limit)
     image_preprocessor = tf.keras.applications.resnet50.preprocess_input
-    return tf.data.TFRecordDataset(files) \
+    dataset = tf.data.TFRecordDataset(files) \
         .map(lambda x: _parse_tfrecord_(x)) \
         .map(lambda x, y: (image_preprocessor(x), y)) \
-        .map(lambda x, y: ({"image": x}, tf.one_hot(y, OUTPUT_SHAPE))) \
         .shuffle(buffer_size=buffer_size) \
-        .repeat() \
         .batch(batch_size) \
         .prefetch(buffer_size=prefetch_batch_num)
+    # Create an iterator
+    iterator = dataset.make_one_shot_iterator()
+    # Create your tf representation of the iterator
+    image, label = iterator.get_next()
+    # Create a one hot array for your labels
+    label = tf.one_hot(label, OUTPUT_SHAPE)
+    return image, label
 
 
 def _parse_tfrecord_(serialized_example):
@@ -147,23 +102,23 @@ if __name__ == '__main__':
     # os.environ["AWS_REGION"] = "ap-southeast-2"
     # os.environ["S3_ENDPOINT"] = "s3.ap-southeast-2.amazonaws.com"
 
-    model_dir = os.environ.get("SM_OUTPUT_DATA_DIR")
+    classifier = build_classifier()
+    classifier.compile(optimizer=Adam(lr=args.learning_rate),
+                       loss=sparse_categorical_crossentropy,
+                       metrics=['accuracy'])
 
-    configuration = tf.estimator.RunConfig(
-        model_dir=model_dir,
-        keep_checkpoint_max=5,
-        save_checkpoints_steps=1,
-        log_step_count_steps=1)  # set the frequency of logging steps for loss function
+    checkpoint_path = args.model_dir + "/checkpoint_classifier.ckpt"
+    checkpoint_dir = args.model_dir
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
+                                                     save_weights_only=True,
+                                                     verbose=1)
 
-    params = {
-        "learning_rate": args.learning_rate
-        # "total_steps": 100
-    }
+    train_x, train_y = _input_fn(args.train)
+    eval_x, eval_y = _input_fn(args.eval)
 
-    estimator = tf.estimator.Estimator(model_fn=model_fn, params=params, config=configuration)
+    classifier.fit(train_x, train_y,
+                   epochs=args.epochs,
+                   validation_data=(eval_x, eval_y),
+                   callbacks=[cp_callback])
 
-    if args.train:
-        estimator.train(input_fn=lambda : _input_fn(args.train), steps=args.epochs)
-    elif args.eval:
-        scores = estimator.evaluate(input_fn=lambda : _input_fn(args.eval), steps=5)
-        print("Evaluation scores: " + scores)
+    classifier.save(args.model_dir + '/trained_model.h5')
